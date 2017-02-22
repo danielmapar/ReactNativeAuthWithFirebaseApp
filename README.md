3244ea863f3a9891ee48575414252ec6e48918b6
Daniel Parreira <daniel@Daniels-MacBook-Pro.local>
2017-02-22 15:41:58 -0300

    initial

---
+++ README.md
@@ -0,0 +1,24 @@
+# React Native - Albums App
+
+  - Setup React Native CLI for the desired platform
+    - https://facebook.github.io/react-native/docs/getting-started.html
+
+  - Install dependencies and start app
+```sh
+$ git clone git@github.com:danielmapar/ReactNativeAuthWithFirebaseApp.git
+$ cd ReactNativeAuthWithFirebaseApp
+$ npm install
+$ touch .env
+$ echo "firebaseApiKey='${FIREBASE_API_KEY}'" > .env
+$ echo "firebaseAuthDomain='${FIREBASE_AUTH_DOMAIN}'" > .env
+$ echo "firebaseDatabaseURL='${FIREBASE_DATABASE_URL'" > .env
+$ echo "firebaseStorageBucket='${FIREBASE_STORAGE_BUCKET}'" > .env
+$ echo "firebaseMessagingSenderId='${FIREBASE_MESSAGING_SENDER_ID}'" > .env
+$ react-native link react-native-config
+$ react-native run-ios OR react-native run-android
+```
+
+  - Optional - Install Linter and ESLint inside Atom to modify this project following
+  the standards inside ```.eslintrc```:
+    - Install linter package
+    - Install linter-eslint package
---
+++ src/app.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import Config from 'react-native-config';
+
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: Config.firebaseApiKey,
+      authDomain: Config.firebaseAuthDomain,
+      databaseURL: Config.firebaseDatabaseURL,
+      storageBucket: Config.firebaseStorageBucket,
+      messagingSenderId: Config.firebaseMessagingSenderId
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
+}
+
+export default App;
